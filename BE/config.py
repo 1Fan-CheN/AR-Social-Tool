0a75cb7d6e8c53f2e803ae0994ba8911919310d6
1Fan-CheN <chenyi0014@126.com>
2022-03-29 01:18:36 +0800

    mod: add pip requirements.txt

--- BE/config.py
+++ BE/config.py
@@ -15,7 +15,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 def create_log(env):
     with open(os.path.join(basedir, './LogConfig.yaml'), 'r') as f:
-        log_config = yaml.load(f.read(), Loader=yaml.FullLoader)
+        try:
+            log_config = yaml.load(f.read(), Loader=yaml.FullLoader)
+        except:
+            log_config = yaml.safe_load(f.read())
     log_config['handlers']['info_file_handler']['filename'] = os.path.join(basedir, 'info.log')
     log_config['handlers']['error_file_handler']['filename'] = os.path.join(basedir, 'error.log')
     logging.config.dictConfig(log_config)
